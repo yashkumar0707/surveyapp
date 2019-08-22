acf6b84c69f345676673c6a36ebadf640f8de399
yashkumar0707 <yashkumar0707@gmail.com>
2019-08-23 00:54:39 +0530

    Created data.js

---
+++ test/data.js
@@ -0,0 +1,37 @@
+var jsonData = [
+    {
+        "q" : "1. Question ?",
+        "opt1" : "A",
+        "opt2" : "B",
+        "opt3" : "C",
+        "answer" : "All of these"
+    },
+    {
+        "q" : "2. Question ?",
+        "opt1" : "A",
+        "opt2" : "B",
+        "opt3" : "C",
+        "answer" : "All of these"
+    },
+    {
+        "q" : "3. Question ?",
+        "opt1" : "A",
+        "opt2" : "B",
+        "opt3" : "C",
+        "answer" : "All of these"
+    },
+    {
+        "q" : "4. Question ?",
+        "opt1" : "A",
+        "opt2" : "B",
+        "opt3" : "C",
+        "answer" : "All of these"
+    },
+    {
+        "q" : "5. Question ?",
+        "opt1" : "A",
+        "opt2" : "B",
+        "opt3" : "C",
+        "answer" : "All of these"
+    }
+];

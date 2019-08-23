20c2eaccfbc6cbbfe71e59a036d92d822321a090
yashkumar0707 <yashkumar0707@gmail.com>
2019-08-24 00:49:56 +0530

    added the final display

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-// json array movement variable
-var i = 0;
-var j=0;
+var i=0;
+var j =0;
+const cafelist= document.querySelector('#sur');
 var correctCount = 0 ;
 //initialize the first question
 generate(0);
@@ -81,27 +81,37 @@ function th(d)
 
 function display()
 {   var j=0;
-    var i=3;
+    var k=3;
+    document.getElementById("question").innerHTML="*****Result*****";
+        document.getElementById("optt1").innerHTML = "";
+        document.getElementById("optt2").innerHTML ="";
+        document.getElementById("optt3").innerHTML = "";
+        document.getElementById("opt1").style.display="none";
+        document.getElementById("opt2").style.display="none";
+        document.getElementById("opt3").style.display="none";
 function rendercafe(doc)
-{
+{   
+    
     let li=document.createElement('li');
     let q=document.createElement('h3');
     let a=document.createElement('span');
     // setting the document id from firestore
-    li.setAttribute('data-id',doc.id);
+    li.setAttribute('data-id', doc.id);
+   
     a.textContent=doc.data().name+" Users chose this option "+doc.data().count;
     //appending
     li.appendChild(a);  
+    cafelist.appendChild(li);
 }
 
 
 db.collection('survey').orderBy('name').get().then((snapshot) =>{
     snapshot.docs.forEach(doc =>{
-        if(i%3==0)
+        if(k%3==0)
         disp();
         console.log(doc.data());
         rendercafe(doc);
-        i++;
+        k++;
     })
     console.log(snapshot.docs);
 })  
@@ -112,19 +122,20 @@ function disp()
 {   
     let li=document.createElement('li');
     let q=document.createElement('h3');
-    let a=document.createElement('span');
-    let b=document.createElement('span');
-    let c=document.createElement('span');
+    let a=document.createElement('div');
+    let b=document.createElement('div');
+    let c=document.createElement('div');
     // setting the document id from firestore
     q.textContent=jsonData[j].q;
-    a.textContent=jsonData[j].a;
-    b.textContent=jsonData[j].b;
-    c.textContent=jsonData[j].c;
+    a.textContent=jsonData[j].opt1;
+    b.textContent=jsonData[j].opt2;
+    c.textContent=jsonData[j].opt3;
     //appending
     j++;
     li.appendChild(q); 
     li.appendChild(a);
     li.appendChild(b); 
     li.appendChild(c);
+    cafelist.appendChild(li);
 }
 }

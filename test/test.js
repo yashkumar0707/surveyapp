2216a706f5b2a299614e2495460a04c571cc3fd6
yashkumar0707 <yashkumar0707@gmail.com>
2019-08-24 00:25:51 +0530

    Updated test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 // json array movement variable
 var i = 0;
+var j=0;
 var correctCount = 0 ;
 //initialize the first question
 generate(0);
@@ -42,11 +43,13 @@ function checkAnswer()
         /*console.log(correctCount);
         document.write("<body style='background-color:#348322;'>");
         document.write("<div style='color:#ffffff;font-size:18pt;text-align:center;'>*****Your score is : "+correctCount+"*****</div>");
-        document.write("</body>");*/
+        document.write("</body>");
         document.getElementById("question").innerHTML="*****Your score is : "+correctCount+"*****";
-        document.getElementById("optt1").innerHTML = jsonData[i].opt1;
-        document.getElementById("optt2").innerHTML = jsonData[i].opt2;
-        document.getElementById("optt3").innerHTML = jsonData[i].opt3;
+        document.getElementById("optt1").innerHTML = "";
+        document.getElementById("optt2").innerHTML ="";
+        document.getElementById("optt3").innerHTML = "";
+        */
+       display();
     }
     else
     {// callback to generate
@@ -60,7 +63,7 @@ function th(d)
     console.log(d);
     db.collection('survey').get().then((snapshot) =>{
     snapshot.docs.forEach(doc =>{
-        if(doc.data().name == d)
+         if(doc.data().name == d)
         {    
             var a=doc.data().count;
             var b=doc.id;
@@ -73,4 +76,55 @@ function th(d)
         }
     })
 })  
+}
+
+
+function display()
+{   var j=0;
+    var i=3;
+function rendercafe(doc)
+{
+    let li=document.createElement('li');
+    let q=document.createElement('h3');
+    let a=document.createElement('span');
+    // setting the document id from firestore
+    li.setAttribute('data-id',doc.id);
+    a.textContent=doc.data().name+" Users chose this option "+doc.data().count;
+    //appending
+    li.appendChild(a);  
+}
+
+
+db.collection('survey').orderBy('name').get().then((snapshot) =>{
+    snapshot.docs.forEach(doc =>{
+        if(i%3==0)
+        disp();
+        console.log(doc.data());
+        rendercafe(doc);
+        i++;
+    })
+    console.log(snapshot.docs);
+})  
+
+
+
+function disp()
+{   
+    let li=document.createElement('li');
+    let q=document.createElement('h3');
+    let a=document.createElement('span');
+    let b=document.createElement('span');
+    let c=document.createElement('span');
+    // setting the document id from firestore
+    q.textContent=jsonData[j].q;
+    a.textContent=jsonData[j].a;
+    b.textContent=jsonData[j].b;
+    c.textContent=jsonData[j].c;
+    //appending
+    j++;
+    li.appendChild(q); 
+    li.appendChild(a);
+    li.appendChild(b); 
+    li.appendChild(c);
+}
 }

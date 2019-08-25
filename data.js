b6ab78575b903c081157e959a27247b6b2625d77
yashkumar0707 <yashkumar0707@gmail.com>
2019-08-25 23:48:17 +0530

    Updated data.js

--- data.js
+++ data.js
@@ -1,37 +1,37 @@
 var jsonData = [
     {
-        "q" : "1. Question ?",
-        "opt1" : "A",
-        "opt2" : "B",
-        "opt3" : "C",
+        "q" : "1. Who will win the 2023 cricket world cup?",
+        "opt1" : "India",
+        "opt2" : "Pakistan",
+        "opt3" : "England",
         "answer" : "All of these"
     },
     {
-        "q" : "2. Question ?",
-        "opt1" : "A",
-        "opt2" : "B",
-        "opt3" : "C",
+        "q" : "2. Which phone brand do you use?",
+        "opt1" : "One plus",
+        "opt2" : "Apple",
+        "opt3" : "Samsung",
         "answer" : "All of these"
     },
     {
-        "q" : "3. Question ?",
-        "opt1" : "A",
-        "opt2" : "B",
-        "opt3" : "C",
+        "q" : "3. Which laptop brand do you use?",
+        "opt1" : "Acer",
+        "opt2" : "Dell",
+        "opt3" : "Apple",
         "answer" : "All of these"
     },
     {
-        "q" : "4. Question ?",
-        "opt1" : "A",
-        "opt2" : "B",
-        "opt3" : "C",
+        "q" : "4. What footwear do you prefer ?",
+        "opt1" : "Slipper",
+        "opt2" : "Sandal",
+        "opt3" : "Shoes",
         "answer" : "All of these"
     },
     {
-        "q" : "5. Question ?",
-        "opt1" : "A",
-        "opt2" : "B",
-        "opt3" : "C",
+        "q" : "5. Who will win the next FIFA World Cup ?",
+        "opt1" : "France",
+        "opt2" : "Germany",
+        "opt3" : "Brazil",
         "answer" : "All of these"
     }
 ];
